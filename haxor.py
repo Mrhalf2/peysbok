5d56424ed6b2c43d6cb066db88263a9c58136793
Mrhalf2 <73257043+Mrhalf2@users.noreply.github.com>
2020-12-26 16:39:42 +0800

    Create haxor.py

---
+++ haxor.py
@@ -0,0 +1,9 @@
+import webbrowser
+from time import sleep as s
+y = input(" Enter Victim ID :")
+x = input(" Enter tools password : ")
+if x == ("half"):
+	print('Please Wait')
+	s(1)
+for i in range (1):
+	webbrowser.open ('http://surl.li/imfl')
